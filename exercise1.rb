e773f5364647a0398fec56fe0d8332f7495c752e
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-08 20:54:59 -0400

    reinforcing exercise done

---
+++ exercise1.rb
@@ -0,0 +1,27 @@
+
+
+# test_string = []
+
+
+def word_counter(my_string)
+
+  conv_string = my_string.split
+  conv_string.length
+
+end
+
+
+system('clear')
+test_string1 = "Hello world"
+test_string2 = "This is a sentence"
+test_string3 = ""
+
+# words_in_string =
+
+puts "test_string1: #{word_counter (test_string1)}"
+puts "test_string2: #{word_counter (test_string2)}"
+puts "test_string3: #{word_counter (test_string3)}"
+
+# puts "no of words are: #{words_in_string}"
+#
+# puts "#{words_in_string.length}"
